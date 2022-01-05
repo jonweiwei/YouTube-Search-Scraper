9bea0546abcb5b027499d7e1637dfdf0d8217b61
Jonathan Wei <90855834+jonweiwei@users.noreply.github.com>
2022-01-04 22:01:37 -0500

    # of comments is finally sorted out
    the problem turned out to be that some videos had comments turned off, in which case the object found by Beautiful Soup turned out to be None, which was causing an error

--- ytscraper.py
+++ ytscraper.py
@@ -7,7 +7,7 @@ def sortByViews(arr):
     print('hi')
 
 
-def main():
+def getData():
     driver = webdriver.Chrome()
     driver.get('https://www.youtube.com/results?search_query=f1')
     time.sleep(2)
@@ -19,34 +19,32 @@ def main():
     channels = soup.findAll('a', class_ = 'yt-simple-endpoint style-scope yt-formatted-string')
     durations = soup.findAll('span', id = 'text')
 
-    #driver.get('https://www.youtube.com{}'.format(urls[0].get('href')))
-    #time.sleep(2)
-    #content = driver.page_source.encode('utf-8').strip()
-    #soup = BeautifulSoup(content, 'lxml')
-    #likes = soup.findAll('a', class_ = 'yt-simple-endpoint style-scope ytd-toggle-button-renderer')
-    #print(likes[0].text)
     views = []
     dates = []
     likes = []
     comments = []
     subs = []
 
-    for i in urls:
-        driver.get('https://www.youtube.com{}'.format(i.get('href')))
+    for i in range(len(titles)):
+        driver.get('https://www.youtube.com{}'.format(urls[i].get('href')))
+        driver.maximize_window()
+        # time.sleep is used here to ensure the scrolling is effective
+        time.sleep(1)
+        # scroll down the page to load the comments
+        driver.execute_script("window.scrollTo(0,600)")
         # time.sleep is used here to ensure that the page has loaded before the data is read
-        # the value should be changed depending on the speed of the computer
+        # the value should be changed depending on the speed of the computer and the network as well
         time.sleep(2)
         content = driver.page_source.encode('utf-8').strip()
         soup = BeautifulSoup(content, 'lxml')
         views.append(soup.find('span', class_ = 'view-count style-scope ytd-video-view-count-renderer'))
         dates.append(soup.find('div', id = 'info-strings'))
         likes.append(soup.find('a', class_ = 'yt-simple-endpoint style-scope ytd-toggle-button-renderer'))
-        comments.append(soup.find('span', class_ = 'style-scope yt-formatted-string'))
+        comments.append(soup.find('yt-formatted-string', class_ = 'count-text style-scope ytd-comments-header-renderer'))
         subs.append(soup.find('yt-formatted-string', id = 'owner-sub-count'))
 
     arr = [[''] * 9 for i in range(len(titles))]
     j = 0
-    k = 1
     for i in range(len(titles)):
         arr[i][0] = titles[i].text.replace('\n', '')
         arr[i][1] = channels[j].text
@@ -55,18 +53,15 @@ def main():
         temp = durations[i].text.replace('\n', '')
         arr[i][4] = temp.replace(' ', '')
         arr[i][5] = likes[i].text
+        if(isinstance(comments[i], type(None))):
+            arr[i][6] = ''
+        else:
+            arr[i][6] = comments[i].text
+        arr[i][7] = subs[i].text
         arr[i][8] = 'https://www.youtube.com{}'.format(urls[i].get('href'))
         j += 2
-        k += 2
     
     for i in arr:
         print(i)
 
-    #for i in durations:
-        #print(i.text)
-    #for i in views:
-        #print(i.text)
-    #print(url.get('href'))
-    
-
-main()
+getData()
