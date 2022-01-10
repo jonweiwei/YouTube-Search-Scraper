5ee0e6b5b1960321e4e77f0f672a6a3141021eed
Jonathan Wei <90855834+jonweiwei@users.noreply.github.com>
2022-01-09 23:24:42 -0500

    set the icon

--- ytscrapergui.py
+++ ytscrapergui.py
@@ -1,9 +1,16 @@
 from ytscraper import *
 from tkinter import *
+from tkinter import ttk
+from os import path
 
 root = Tk()
+root.title('YouTube Search Web Scraper')
+icon = path.dirname(path.realpath(__file__)) + '\images\icon.ico'
+root.iconbitmap(icon)
 
-array = getData()
-array = sortBySubsReverse(array)
-for i in range(3):
-    print(array[i])
+#array = getData('f1')
+#array = sortByViews(array)
+#for i in range(3):
+#    print(array[i])
+
+root.mainloop()
