418fd2f9453eeedf264297e10bb5e5334acec414
Jonathan Wei <90855834+jonweiwei@users.noreply.github.com>
2022-01-11 21:53:16 -0500

    added treeview

--- ytscrapergui.py
+++ ytscrapergui.py
@@ -9,6 +9,11 @@ icon = path.dirname(path.realpath(__file__)) + '\images\icon.ico'
 root.iconbitmap(icon)
 root.geometry("800x600")
 
+table = ttk.Treeview(root)
+
+table['columns'] = ("Title", "Channel", "Views", "Date", "Duration", "Comments", "Channel Subscribers", "URL")
+table.column("#0", width = 0)
+
 #array = getData('f1')
 #array = sortByViews(array)
 #for i in range(3):
